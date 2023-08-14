8711935d5fcb5a9416970e697bbcc81b5546366b
jamoliddin <sh.jamoliddin2001@gmail.com>
2023-08-14 10:11:29 +0500

    - update

--- lesson1_tasks.go
+++ lesson1_tasks.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
 
 	/*for index, value := range array {
@@ -25,10 +23,12 @@ func main() {
 	// #2 Дан массив ненулевых целых чисел размера N. Проверить, чередуются ли в нем положительные и отрицательные числа.
 	// Если чередуются, то вывести 0, если нет, то вывести порядковый номер первого элемента, нарушающего закономерность.
 
-	/*var array = []int{1, -4, 3, -432, 6, -6, 234, -2, 3, -4, 4, -2, 2}
+	/*var array = []int{1, -4, 3, -432, -6, -6, 234, -2, 3, -4, 4, -2, 2}
 	for i := 0; i < len(array)-1; i++ {
+		fmt.Println("i = ", i, " array[", i, "] = ", array[i], " array[", i+1, "] = ", array[i+1])
 		if array[i]*array[i+1] > 0 {
-			fmt.Println(i + 1)
+			fmt.Println("if check")
+			fmt.Println("i = ", i+1)
 			return
 		}
 	}
@@ -37,7 +37,7 @@ func main() {
 	// #3 Дан целочисленный массив размера N, все элементы которого упорядочены (по возрастанию или по убыванию).
 	// Найти количество различных элементов в данном массиве.
 
-	/*var array = []int{8, 8, 6, 6, 6, 6, 3, 3, 3, 1, 1, 0, -2}
+	/*var array = []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1}
 	k := 0
 	for i := 0; i < len(array)-1; i++ {
 		if array[i] != array[i+1] {
@@ -50,10 +50,13 @@ func main() {
 	// #4 Дан массив размера N, все элементы которого, кроме последнего, упорядочены по возрастанию.
 	// Сделать массив упорядоченным, переместив последний элемент на новую позицию.
 
-	/*var array = []int{1, 2, 4, 6, 6, 6, 7, 8, 11, 12, 12, 14, 3}
+	/*var array = []int{1, 2, 4, 6, 6, 6, 7, 8, 11, 12, 12, 14, 17} // 3, 4, 6, 6, 6, 7, 8, 11, 12, 12, 14
 	lastElement := array[len(array)-1]
 	pos := 0
-	for lastElement > array[pos] && pos < len(array)-1 {
+	for pos < len(array)-1 {
+		if lastElement <= array[pos] {
+			break
+		}
 		pos++
 	}
 	for i := len(array) - 1; i > pos; i-- {
@@ -62,7 +65,7 @@ func main() {
 	array[pos] = lastElement
 	fmt.Println(array)*/
 
-	var myArray = []int{1, 3, 4, 6, 1, 3, 2, 5, 7, -2, 6}
+	/*var myArray = []int{1, 3, 4, 6, 1, 3, 2, 5, 7, -2, 6}
 	for i := 0; i < len(myArray)-1; i++ {
 		for j := i + 1; j < len(myArray); j++ {
 			if myArray[i] > myArray[j] {
@@ -73,6 +76,8 @@ func main() {
 		}
 	}
 
-	fmt.Println(myArray)
+	fmt.Println(myArray)*/
+
+	// refresh
 
 }
