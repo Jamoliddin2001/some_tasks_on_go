cc709b669ba025eb73beda936f20aaeb8492e696
jamoliddin <sh.jamoliddin2001@gmail.com>
2023-08-14 11:08:41 +0500

    - add recursion

--- func.go
+++ func.go
@@ -12,6 +12,7 @@ func main() {
 	fmt.Println(add(4, 5))
 	fmt.Println(returnTwoParams(14, 15, "First", "Last"))
 	fmt.Println(multiply(4, 7))
+	fmt.Println(factorial(5))
 }
 
 func add(x, y int) int {
@@ -27,4 +28,13 @@ func returnTwoParams(x, y int, firstName, lastName string) (int, string) {
 func multiply(x, y int) (z int) {
 	z = x * y
 	return
+}
+
+// recursion
+func factorial(n uint) uint {
+
+	if n == 0 {
+		return 1
+	}
+	return n * factorial(n-1)
 }
