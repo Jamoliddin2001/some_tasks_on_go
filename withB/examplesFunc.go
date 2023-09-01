88af46eabca94311acf7723f9b9654a0633f607b
jamoliddin <sh.jamoliddin2001@gmail.com>
2023-09-02 00:11:23 +0500

    - add dunc getRightTime

--- withB/examplesFunc.go
+++ withB/examplesFunc.go
@@ -1,9 +1,29 @@
 package main
 
-func perimetr(a float64, b float64) {
+import "strconv"
 
+func getRightTime(sec int) string {
+	var hh = sec / 3600
+	sec = sec - hh*3600
+	var mm = sec / 60
+	sec = sec - mm*60
+	var s = ""
+	if hh < 10 {
+		s += "0"
+	}
+	s += strconv.Itoa(hh) + ":"
+	if mm < 10 {
+		s += "0"
+	}
+	s += strconv.Itoa(mm) + ":"
+	if sec < 10 {
+		s += "0"
+	}
+	s += strconv.Itoa(sec)
+	return s
 }
 
 func main() {
-
+	time := getRightTime(43253)
+	println(time)
 }
